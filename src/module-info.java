a05827a316517e032043950d3be36d3fed2d940c
Keon Bushman <138335528+IPFizzy@users.noreply.github.com>
2026-08-17 16:14:36 -0700

    Rename Java module identity

--- src/module-info.java
+++ src/module-info.java
@@ -1,11 +1,11 @@
-open module CST239Milestone {
+open module fantasy.storefront {
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.annotation;
 
     requires org.junit.jupiter.api;
     requires org.junit.jupiter.engine;
-    
+
     exports app;
     exports model;
     exports service;
